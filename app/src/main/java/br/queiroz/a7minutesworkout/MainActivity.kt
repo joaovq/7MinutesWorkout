17501c5275a743ac4ca7541ab79524e6a89e7bb2
joaovq <vitorsnta90@gmail.com>
2023-01-25 14:03:33 -0300

    add splash screen (This isn't in the project at Udemy)

--- app/src/main/java/br/queiroz/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/br/queiroz/a7minutesworkout/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setTheme(R.style.Theme_7MinutesWorkout)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
