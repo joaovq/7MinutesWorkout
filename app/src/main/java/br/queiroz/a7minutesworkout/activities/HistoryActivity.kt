838e071685b601491fdd1b7d799690e5bc08ca37
joaovq <vitorsnta90@gmail.com>
2023-02-02 18:52:57 -0300

    finish project

--- app/src/main/java/br/queiroz/a7minutesworkout/activities/HistoryActivity.kt
+++ app/src/main/java/br/queiroz/a7minutesworkout/activities/HistoryActivity.kt
@@ -2,10 +2,14 @@ package br.queiroz.a7minutesworkout.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import br.queiroz.a7minutesworkout.WorkOutApp
+import br.queiroz.a7minutesworkout.adapter.HistoryAdapter
 import br.queiroz.a7minutesworkout.data.HistoryDao
+import br.queiroz.a7minutesworkout.data.HistoryEntity
 import br.queiroz.a7minutesworkout.databinding.ActivityHistoryBinding
 import kotlinx.coroutines.launch
 
@@ -39,10 +43,30 @@ class HistoryActivity : AppCompatActivity() {
     private fun getAllCompletedDates(historyDao: HistoryDao) {
         lifecycleScope.launch {
             historyDao.fetchAllHistories().collect {
-                it.forEach { history ->
-                    Log.e("History Entity - ", history.toString())
+                history->
+                if (history.isNotEmpty()){
+                    val historyEntities = ArrayList(history)
+                    setUpRecyclerViewHistories(historyEntities)
+                }else{
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
+                    binding?.tvHistory?.visibility = View.INVISIBLE
+                    binding?.rvHistory?.visibility = View.INVISIBLE
                 }
             }
+        }
+    }
+
+    private fun setUpRecyclerViewHistories(histories:ArrayList<HistoryEntity>) {
+        binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+        binding?.tvHistory?.visibility = View.VISIBLE
+
+        binding?.rvHistory?.apply {
+            visibility = View.VISIBLE
+            val snapHelper = LinearSnapHelper()
+            snapHelper.attachToRecyclerView(this)
+            layoutManager = LinearLayoutManager(this@HistoryActivity)
+            setHasFixedSize(false)
+            adapter = HistoryAdapter(histories)
         }
     }
 
